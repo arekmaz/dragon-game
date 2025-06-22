42b5041f478d83ebef127ee06f4a8581ae03a578
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2025-06-22 14:06:23 +0200

    --wip-- [skip ci]

--- src/Cli.ts
+++ src/Cli.ts
@@ -1,9 +1,20 @@
-import { Command } from "@effect/cli";
-import { Layer, Logger, LogLevel } from "effect";
+import { Command, Options } from "@effect/cli";
+import { Effect, Layer, Logger, LogLevel } from "effect";
 import { GameCmd } from "./game.ts";
 
-const command = Command.make("dragon", {}, () =>
-  Logger.withMinimumLogLevel(Layer.launch(GameCmd.Default), LogLevel.Debug)
+const command = Command.make(
+  "dragon",
+  {
+    debug: Options.boolean("debug", { aliases: ["d"], ifPresent: true }),
+  },
+  Effect.fn("dragon-game")(function* ({ debug }) {
+    return yield* Effect.gen(function* () {
+      if (debug) {
+        yield* Effect.logDebug("Debug mode enabled");
+      }
+      return yield* Layer.launch(GameCmd.Default);
+    }).pipe(Logger.withMinimumLogLevel(debug ? LogLevel.Debug : LogLevel.Info));
+  })
 );
 
 export const run = Command.run(command, {
